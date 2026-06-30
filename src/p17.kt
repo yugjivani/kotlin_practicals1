d2bf3297b092af10d0a5a27a4acc211bcbeb751c
LENOVA <ydjivani421@gmail.com>
2026-06-30 14:02:18 +0530

    i

--- src/p17.kt
+++ src/p17.kt
@@ -1,5 +1,12 @@
-fun main(){
+fun main() {
     println("Enter your Number:")
-    val num = readln().toInt()
-
+    val number = readln().toInt()
+    val result = fact(number)
+    println("Factorial of $number is: $result")
+}
+fun fact(n: Int): Int {
+    if (n <= 1) {
+        return 1
+    }
+    return n * fact(n - 1)
 }
